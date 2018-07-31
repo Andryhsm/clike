ebe08043886301d9ddd862644f3c2862c91d9ff2
Lydia Andrianjatovo <lydiaandrianjatovo@gmail.com>
2018-07-31 15:21:33 +0300

    Envoie des fichiers complémentaires dans le fonctionnement de newsletter compte client

---
+++ app/Models/NewsletterOption.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class NewsletterOption extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $table = 'newsletter_option';
+    protected $primaryKey = 'newsletter_option_id';
+    protected $fillable = ['user_id', 'key', 'value'];
+
+    public function user()
+	{
+		return $this->hasOne(User::class,'user_id','user_id');
+	}
+}
