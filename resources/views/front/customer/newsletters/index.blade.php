fb230bc211a15cd5bc1a6c7f040067f2b2b5cce9
Lydia Andrianjatovo <lydiaandrianjatovo@gmail.com>
2018-07-31 15:31:19 +0300

    Correction de la newsletter s'il n'y a pas encore de configuration présente

--- resources/views/front/customer/newsletters/index.blade.php
+++ resources/views/front/customer/newsletters/index.blade.php
@@ -15,17 +15,31 @@
                     Dernières tendances, nouveautés et conseils : vous avez vu les produits en premier, vous serez les premiers à les avoir
                 </p>
             </div>
-            <div class="newsletter-item checkbox col-lg-3 col-md-3 col-sm-3 col-xs-12">
-                <p>
-                    <a onclick="updateNewsletter(this, 'news', 'e-mail');" id="new-received-email" class="received-email"><i class="fa {!! ($newsletter_option['news']->value == 'e-mail') ? 'fa-dot-circle-o' : 'fa-circle-o' !!}"></i>&nbsp;&nbsp;E-mail</a>
-                </p>
-                <p>
-                    <a onclick="updateNewsletter(this, 'news', 'sms');" id="new-received-notification" class="received-notification"><i class="fa {!! ($newsletter_option['news']->value == 'sms') ? 'fa-dot-circle-o' : 'fa-circle-o' !!}"></i>&nbsp;&nbsp;SMS</a>
-                </p>
-                <p>
-                    <a onclick="updateNewsletter(this, 'news', 'desactivate');"  id="new-desactivate" class="desactivate"><i class="fa {!! ($newsletter_option['news']->value == 'desactivate') ? 'fa-dot-circle-o' : 'fa-circle-o' !!}"></i>&nbsp;&nbsp;Désactiver</a>
-                </p>               
-            </div>
+            @if($newsletter_option)
+                <div class="newsletter-item checkbox col-lg-3 col-md-3 col-sm-3 col-xs-12">
+                    <p>
+                        <a onclick="updateNewsletter(this, 'news', 'e-mail');" id="new-received-email" class="received-email"><i class="fa {!! ($newsletter_option['news']->value == 'e-mail') ? 'fa-dot-circle-o' : 'fa-circle-o' !!}"></i>&nbsp;&nbsp;E-mail</a>
+                    </p>
+                    <p>
+                        <a onclick="updateNewsletter(this, 'news', 'sms');" id="new-received-notification" class="received-notification"><i class="fa {!! ($newsletter_option['news']->value == 'sms') ? 'fa-dot-circle-o' : 'fa-circle-o' !!}"></i>&nbsp;&nbsp;SMS</a>
+                    </p>
+                    <p>
+                        <a onclick="updateNewsletter(this, 'news', 'desactivate');"  id="new-desactivate" class="desactivate"><i class="fa {!! ($newsletter_option['news']->value == 'desactivate') ? 'fa-dot-circle-o' : 'fa-circle-o' !!}"></i>&nbsp;&nbsp;Désactiver</a>
+                    </p>               
+                </div>
+            @else
+                <div class="newsletter-item checkbox col-lg-3 col-md-3 col-sm-3 col-xs-12">
+                    <p>
+                        <a onclick="updateNewsletter(this, 'news', 'e-mail');" id="new-received-email" class="received-email"><i class="fa fa-circle-o"></i>&nbsp;&nbsp;E-mail</a>
+                    </p>
+                    <p>
+                        <a onclick="updateNewsletter(this, 'news', 'sms');" id="new-received-notification" class="received-notification"><i class="fa fa-circle-o"></i>&nbsp;&nbsp;SMS</a>
+                    </p>
+                    <p>
+                        <a onclick="updateNewsletter(this, 'news', 'desactivate');"  id="new-desactivate" class="desactivate"><i class="fa fa-circle-o"></i>&nbsp;&nbsp;Désactiver</a>
+                    </p>               
+                </div>
+            @endif
         </div>
     </div>
     <div class="newsletter col-lg-12 col-md-12 col-sm-12 col-xs-12">
@@ -39,17 +53,31 @@
                     Soyez les premiers à réserver vos coups de coeurs pour moins cher.
                 </p>
             </div>
-            <div class="newsletter-item checkbox col-lg-3 col-md-3 col-sm-3 col-xs-12">
-                <p>
-                    <a onclick="updateNewsletter(this, 'promo_sold', 'e-mail');" id="special-received-email" class="received-email"><i class="fa {!! ($newsletter_option['promo_sold']->value == 'e-mail') ? 'fa-dot-circle-o' : 'fa-circle-o' !!}"></i>&nbsp;&nbsp;E-mail</a>
-                </p>
-                <p>
-                    <a onclick="updateNewsletter(this, 'promo_sold', 'sms');" id="special-received-notification" class="received-notification"><i class="fa {!! ($newsletter_option['promo_sold']->value == 'sms') ? 'fa-dot-circle-o' : 'fa-circle-o' !!}"></i>&nbsp;&nbsp;SMS</a>
-                </p>
-                <p>
-                    <a onclick="updateNewsletter(this, 'promo_sold', 'desactivate');"  id="special-desactivate" class="desactivate"><i class="fa {!! ($newsletter_option['promo_sold']->value == 'desactivate') ? 'fa-dot-circle-o' : 'fa-circle-o' !!}"></i>&nbsp;&nbsp;Désactiver</a>
-                </p>
-            </div>
+            @if($newsletter_option)
+                <div class="newsletter-item checkbox col-lg-3 col-md-3 col-sm-3 col-xs-12">
+                    <p>
+                        <a onclick="updateNewsletter(this, 'promo_sold', 'e-mail');" id="special-received-email" class="received-email"><i class="fa {!! ($newsletter_option['promo_sold']->value == 'e-mail') ? 'fa-dot-circle-o' : 'fa-circle-o' !!}"></i>&nbsp;&nbsp;E-mail</a>
+                    </p>
+                    <p>
+                        <a onclick="updateNewsletter(this, 'promo_sold', 'sms');" id="special-received-notification" class="received-notification"><i class="fa {!! ($newsletter_option['promo_sold']->value == 'sms') ? 'fa-dot-circle-o' : 'fa-circle-o' !!}"></i>&nbsp;&nbsp;SMS</a>
+                    </p>
+                    <p>
+                        <a onclick="updateNewsletter(this, 'promo_sold', 'desactivate');"  id="special-desactivate" class="desactivate"><i class="fa {!! ($newsletter_option['promo_sold']->value == 'desactivate') ? 'fa-dot-circle-o' : 'fa-circle-o' !!}"></i>&nbsp;&nbsp;Désactiver</a>
+                    </p>
+                </div>
+            @else
+                <div class="newsletter-item checkbox col-lg-3 col-md-3 col-sm-3 col-xs-12">
+                    <p>
+                        <a onclick="updateNewsletter(this, 'promo_sold', 'e-mail');" id="special-received-email" class="received-email"><i class="fa fa-circle-o"></i>&nbsp;&nbsp;E-mail</a>
+                    </p>
+                    <p>
+                        <a onclick="updateNewsletter(this, 'promo_sold', 'sms');" id="special-received-notification" class="received-notification"><i class="fa fa-circle-o"></i>&nbsp;&nbsp;SMS</a>
+                    </p>
+                    <p>
+                        <a onclick="updateNewsletter(this, 'promo_sold', 'desactivate');"  id="special-desactivate" class="desactivate"><i class="fa fa-circle-o"></i>&nbsp;&nbsp;Désactiver</a>
+                    </p>
+                </div>
+            @endif
         </div>
     </div>
     <div class="newsletter col-lg-12 col-md-12 col-sm-12 col-xs-12">
@@ -63,17 +91,21 @@
                     Des exclusivités sur-mesure, qui vous ressemblent, élaborées en fonction de vos précédants achats.
                 </p>
             </div>
-            <div class="newsletter-item checkbox col-lg-3 col-md-3 col-sm-3 col-xs-12">
-                <p>
-                    <a onclick="updateNewsletter(this, 'personal', 'e-mail');" id="exclusive-received-email" class="received-email"><i class="fa {!! ($newsletter_option['personal']->value == 'e-mail') ? 'fa-dot-circle-o' : 'fa-circle-o' !!}"></i>&nbsp;&nbsp;E-mail</a>
-                </p>
-                <p>
-                    <a onclick="updateNewsletter(this, 'personal', 'sms');" id="exclusive-received-notification" class="received-notification"><i class="fa {!! ($newsletter_option['personal']->value == 'sms') ? 'fa-dot-circle-o' : 'fa-circle-o' !!}"></i>&nbsp;&nbsp;SMS</a>
-                </p>
-                <p>
-                    <a onclick="updateNewsletter(this, 'personal', 'desactivate');"  id="exclusive-desactivate" class="desactivate"><i class="fa {!! ($newsletter_option['personal']->value == 'desactivate') ? 'fa-dot-circle-o' : 'fa-circle-o' !!}"></i>&nbsp;&nbsp;Désactiver</a>
-                </p>
-            </div>
+            @if($newsletter_option)
+                <div class="newsletter-item checkbox col-lg-3 col-md-3 col-sm-3 col-xs-12">
+                    <p>
+                        <a onclick="updateNewsletter(this, 'personal', 'e-mail');" id="exclusive-received-email" class="received-email"><i class="fa fa-circle-o"></i>&nbsp;&nbsp;E-mail</a>
+                    </p>
+                    <p>
+                        <a onclick="updateNewsletter(this, 'personal', 'sms');" id="exclusive-received-notification" class="received-notification"><i class="fa fa-circle-o"></i>&nbsp;&nbsp;SMS</a>
+                    </p>
+                    <p>
+                        <a onclick="updateNewsletter(this, 'personal', 'desactivate');"  id="exclusive-desactivate" class="desactivate"><i class="fa fa-circle-o"></i>&nbsp;&nbsp;Désactiver</a>
+                    </p>
+                </div>
+            @else
+
+            @endif
         </div>
     </div>
 
