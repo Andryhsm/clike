d0ac737b02c671d29a5d6dd5d009f4f042350276
AndryHSM <aa@alternateeve.com>
2018-08-03 14:25:36 +0200

    recherche zone

--- resources/views/front/modals/search_product.blade.php
+++ resources/views/front/modals/search_product.blade.php
@@ -7,10 +7,15 @@
         </button>
       </div>
       <div class="modal-body">
-            <form method="GET" id="form-search" action="{!! route('search') !!}">   
-               <input type="text" id="search-product-input" name="q" placeholder="Chercher ..." name="search-product">
-               <button type="submit" class="icon btn icon-search search-product btn-search"></button>
+          <div class="row">
+            <form class="col-lg-12 col-md-12 col-sm-12 col-xs-12" method="GET" id="form-search" action="{!! route('search') !!}">   <!-- <div > -->
+                  <input class="col-lg-11 col-md-11 col-sm-11 col-xs-11" type="text" id="search-product-input" name="q" placeholder="Chercher ..." name="search-product">
+               <!-- </div> -->
+               <!-- <div > -->
+                 <button type="submit" class="col-lg-1 col-md-1 col-sm-1 col-xs-1 icon btn icon-search search-product btn-search"></button>
+               <!-- </div> -->
             </form>
+          </div>  
       </div>
     </div>
   </div>
