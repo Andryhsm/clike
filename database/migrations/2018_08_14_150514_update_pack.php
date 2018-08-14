b7c0d939470e6caa603959dfa24aad4e43bf24a8
Andry Andrianaivo <aa@alternateeve.com>
2018-08-14 17:05:56 +0200

    pack update migration

--- database/migrations/2018_08_14_150514_update_pack.php
+++ database/migrations/2018_08_14_150514_update_pack.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class UpdateTablePackForPermissionToMerchant extends Migration
+class UpdatePack extends Migration
 {
     /**
      * Run the migrations.
