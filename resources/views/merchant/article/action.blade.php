1668a47aa3ce4068397ac849b49de98b3d6fdb15
sitraka <sitraka131090@gmail.com>
2018-08-16 20:33:55 +0300

    compte marchand->mes produits->icone modifier,supprimer

--- resources/views/merchant/article/action.blade.php
+++ resources/views/merchant/article/action.blade.php
@@ -1,11 +1,17 @@
 <div class="btn-group">
-	<a href="{!! route('article.edit',['article'=>$product->product_id]) !!}" class="show_article_eye"></a>
+	<a href="{!! route('article.edit',['article'=>$product->product_id]) !!}" class="btn show_article_eye"></a>
     
     {!! Form::open(array('url' => route('article.destroy',['article'=>$product->product_id]), 'class' => 'pull-right')) !!}
     {!! Form::hidden('_method', 'DELETE') !!}
 	
-	<a class="delete_article_garbage">
-    {!! Form::button('', ['type' => 'submit', 'class' => ''.$product->product_id.' hidden'] ) !!}
+	<a class="btn delete_article_garbage">
     </a>
+    {!! Form::button('', ['type' => 'submit', 'class' => 'delete-btn delete-btn'.$product->product_id.' hidden'] ) !!}
     {{ Form::close() }}
 </div>
+
+<script type="text/javascript">
+	$('.delete_article_garbage').on('click', function() {
+    	$(this).parent().find(':submit').trigger('click');
+    });
+</script>
