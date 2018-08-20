3db48cb48a9a783da455535f4bcb9ba137c4d0d5
Lydia Andrianjatovo <lydiaandrianjatovo@gmail.com>
2018-08-20 09:13:39 +0300

    Tablet

---
+++ resources/views/front/pages/mobile_tablet_pages/mobile_engagement_menu.blade.php
@@ -0,0 +1,71 @@
+
+<div class="engagement-menu text-center" id="browseTab"> <!-- hidden -->
+    <br><br><br>
+        
+    <div class="panel-group" id="accordion">
+        <div class="panel panel-default">
+            <div class="row bg-white">  <!-- panel-heading -->
+                <a class="active  text-center col-xs-10" href = "#discovery" data-tab="uploadTab" onclick = "showEngagement(this)">
+                    <div class="pt-10">
+                        <span>Découverte</span>
+                    </div>
+                    <div class="engagement-price mt-25">GRATUIT</div>
+                </a>
+                <i class="fa fa-angle-down"></i>
+            </div>
+            <div id="collapseOne" class="panel-collapse collapse">
+                <div class="panel-body">
+                    <p>HTML stands for HyperText Markup Language. HTML is the standard markup language for describing the structure of web pages. <a href="https://www.tutorialrepublic.com/html-tutorial/" target="_blank">Learn more.</a></p>
+                </div>
+            </div>
+        </div>
+        <div class="panel panel-default">
+            <div class="row bg-white shadow-engagement">
+                <a class="text-center col-xs-10" href = "#seller" data-tab="uploadTab" onclick = "showEngagement(this)">
+                    <div class="pt-10">
+                        <span>Vendeur</span>
+                    </div>
+                    <div class="engagement-price text-center"><span class="price-int">30</span><span class="price-unity">€</span>/mois</div>
+                </a>
+                <i class="fa fa-angle-down"></i>
+            </div>
+            <div id="collapseTwo" class="panel-collapse collapse">
+                <div class="panel-body">
+                    <p>Bootstrap is a sleek, intuitive, and powerful front-end framework for faster and easier web development. It is a collection of CSS and HTML conventions. <a href="https://www.tutorialrepublic.com/twitter-bootstrap-tutorial/" target="_blank">Learn more.</a></p>
+                </div>
+            </div>
+        </div>
+        <div class="panel panel-default">
+            <div class="row bg-white">
+                <a class="text-center col-xs-10" href = "#marketing" data-tab="uploadTab" onclick = "showEngagement(this)">
+                    <div class="pt-10">
+                        <span>Marketing</span>
+                    </div>
+                    <div class="engagement-price text-center"><span class="price-int">45</span><span class="price-unity">€</span>/mois</div>
+                </a>
+                <i class="fa fa-angle-down"></i>
+            </div>
+            <div id="collapseThree" class="panel-collapse collapse">
+                <div class="panel-body">
+                    <p>CSS stands for Cascading Style Sheet. CSS allows you to specify various style properties for a given HTML element such as colors, backgrounds, fonts etc. <a href="https://www.tutorialrepublic.com/css-tutorial/" target="_blank">Learn more.</a></p>
+                </div>
+            </div>
+        </div>   
+        <div class="panel panel-default">
+            <div class="row bg-white">
+                 <a class="text-center col-xs-10" href = "#marketingp" data-tab="uploadTab" onclick = "showEngagement(this)">
+                    <div class="pt-10">
+                        <span>Marketing +</span>
+                    </div>
+                    <div class="engagement-price text-center"><span class="price-int">65</span><span class="price-unity">€</span>/mois</div>                             
+                </a>   
+                <i class="fa fa-angle-down"></i>             
+            </div>
+            <div id="collapseThree" class="panel-collapse collapse">
+                <div class="panel-body">
+                    <p>CSS stands for Cascading Style Sheet. CSS allows you to specify various style properties for a given HTML element such as colors, backgrounds, fonts etc. <a href="https://www.tutorialrepublic.com/css-tutorial/" target="_blank">Learn more.</a></p>
+                </div>
+            </div>
+        </div>  
+    </div>
+</div>
