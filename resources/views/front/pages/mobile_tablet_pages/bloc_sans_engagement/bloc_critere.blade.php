44ab63579aebd785a6e0fec5cf65460a92f9b718
sitraka <sitraka131090@gmail.com>
2018-08-21 21:08:05 +0300

    fractionner par bloc de fichier

---
+++ resources/views/front/pages/mobile_tablet_pages/bloc_sans_engagement/bloc_critere.blade.php
@@ -0,0 +1,12 @@
+<div class="col-lg-2 col-md-2 col-sm-2 col-xs-5 point5 left-header  border-green" id="left-header" style="margin-top: 30rem;">
+    <p><b>Compte employés</b></p>
+    <div class="row bg-white p-0"><p><b>Nombre d’articles</b></p></div>
+    <p><b>Base de données clients</b></p>
+    <div class="row bg-white p-0"><p><b>Visibilité des produits</b></p></div>
+    <p><b>Frais de transaction</b></p>
+    <div class="row bg-white p-0"><p><b>Codes promo</b></p></div>
+    <p><b>Encaissement physique</b></p>
+    <div class="row bg-white p-0"><p><b>Rapport d’activité</b></p></div>
+    <p><b>Fiche boutique</b></p>
+    <div class="row bg-white p-0"><p><b>Newsletters</b></p></div>
+</div>
---
+++ resources/views/front/pages/mobile_tablet_pages/bloc_sans_engagement/marketing_plus.blade.php
@@ -0,0 +1,32 @@
+    <div class="col-lg-2 col-md-2 col-sm-2 col-xs-7 point5 border-green"  id="marketingp">
+        <div class="row bg-white header-engagement-height">
+            <div class="title pb-20 pt-10">
+                <img class="engagement-logo" src="{!! URL::to('/') !!}/images/icon/logo_dans_page.svg" alt="logo clickee"/>
+                <span>Marketing +</span>
+            </div>
+            <div class="engagement-price mb--10"><span class="price-int">{!! showPackByValue($packs,'Marketing +','Engagement annuel','price') !!}</span><span class="price-unity">€</span>/mois</div>
+            <a href="#" class="btn btn-clickee-info-plein mtb-10">CHOISIR</a>
+
+            <span>
+                                    Souscrivez au Pack Marketing + quand votre base de données clients grandie.
+                                </span>
+        </div>
+        <p>illimité</p>
+        <div class="row bg-white p-0"><p>illimité</p></div>
+        <p>illimité</p>
+        <div class="row bg-white p-0">
+            <p><i class="fa fa-check"></i></p>
+        </div>
+        <p>3,5%</p>
+        <div class="row bg-white p-0">
+            <p><i class="fa fa-check"></i></p>
+        </div>
+        <p><i class="fa fa-check"></i></p>
+        <div class="row bg-white p-0">
+            <p><i class="fa fa-check"></i></p>
+        </div>
+        <p><i class="fa fa-check"></i></p>
+        <div class="row bg-white p-0">
+            <p>de 500 à 2 000 clients</p>
+        </div>
+    </div>
