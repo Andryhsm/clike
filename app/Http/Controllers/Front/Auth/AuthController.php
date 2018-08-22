d213c6ef8a478e5ac9c92f1a50f7ea93bc7a7c9e
Andry Andrianaivo <aa@alternateeve.com>
2018-08-22 09:31:36 +0200

    test twiter2

--- app/Http/Controllers/Front/Auth/AuthController.php
+++ app/Http/Controllers/Front/Auth/AuthController.php
@@ -190,7 +190,7 @@ class AuthController extends Controller
 
     public function redirectToProvider($provider)
     {
-        dd(Socialite::driver($provider)->redirect());
+        dd(Socialite::driver('twitter')->redirect());
         return Socialite::driver($provider)->redirect();
     }
 
