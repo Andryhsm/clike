2c45653748a9a3fb78f0f286175e374b69cb2f98
Andry Andrianaivo <aa@alternateeve.com>
2018-08-29 16:17:35 +0200

    commande dans commercant

--- app/Http/Controllers/Front/Merchant/OrderController.php
+++ app/Http/Controllers/Front/Merchant/OrderController.php
@@ -62,7 +62,7 @@ class OrderController extends Controller
         $items = [];
         $b=false;
         $d=false;
-        dd($order_items);
+        //dd($order_items);
         //chercher les items
         foreach ($order_items as $item) {
             $country = $item->product->store->country->name;
